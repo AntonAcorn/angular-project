586bb3f1c3a5dcc53edbcfe7a727d9c60d23cb56
AntonAcorn <ShustikovAnimation@gmail.com>
2024-05-25 10:58:56 -0400

    #Add emitter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, input, computed, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,15 +10,23 @@ export class UserComponent {
 // @Input({required: true}) avatar!: string;
 //@Input({required: true}) name!: string;
 // The input works as a signal, so in HTML this field needs to be called as a function with ().
+@Input({required: true}) id!: string;
 name = input.required<string>();
 avatar = input.required<string>();
 
+@Output() select = new EventEmitter();
 
-get imagePath() {
+// Лучше использовать computed, потому что Angular будет пересчитывать только при изменении данных в avatar
+imagePath = computed(() => {
 	return 'assets/users/' + this.avatar();
-}
+})
+
+// Геттер вызывается как свойство, а не как функция
+// get imagePath() {
+// 	return 'assets/users/' + this.avatar();
+// }
 
   onSelectUser() {
-   
+   this.select.emit(this.id);
   }
 }
