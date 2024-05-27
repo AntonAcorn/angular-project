3663198264a60dfc55d5999e41d48d443a04667a
AntonAcorn <ShustikovAnimation@gmail.com>
2024-05-27 19:15:03 -0400

    #Add task component to tasks

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,10 +1,20 @@
 import { Component, Input } from '@angular/core';
 
+interface Task {
+	id: string;
+	userId: string;
+	title: string;
+	summary: string;
+	dueDate: string;
+}
+
 @Component({
   selector: 'app-task',
   standalone: true,
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
-export class TasksComponent {
+export class TaskComponent {
+
+	@Input({required: true}) task!: Task;
 }
